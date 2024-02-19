923763efcd20e1f0409e2dc32024da3cd46f7845
patrickmuchiri <pmuchirig@gmail.com>
2024-02-19 11:54:28 -0800

    a script that prepares a MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- A script that creates the database hbnb_test_db
+-- creates a user and grants priviledges
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
