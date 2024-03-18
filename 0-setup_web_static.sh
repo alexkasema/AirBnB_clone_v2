74e123a3d83f7abf2d0059a26fda9e2fda67e958
alexkasema <lexxmulwa@gmail.com>
2024-03-18 07:52:15 -0700

    modified location in the sites-available alexkasema.tech file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,5 +14,5 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "35i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "35i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/alexkasema.tech
 sudo service nginx restart
