40c8688d460987b3ed266c53da1dbdc3f61ac697
Alex Kasema <lexxmulwa@gmail.com>
2024-03-21 03:19:09 -0700

    Modified the echo command to write output to file using | sudo tee

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,7 +11,7 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "35i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/alexkasema.tech
